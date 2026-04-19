920275e5918228c49cf68aa9530d7b8efcee9338
Shubham Rawat <shubhamrawat@Shubhams-MacBook-Air.local>
2026-04-19 14:21:01 +0530

    commiting

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -34,15 +34,12 @@ func createRandomAccount(t *testing.T) Account {
 		Balance:  util.RandomMoney(),
 		Currency: util.RandomCurrency(),
 	}
-
 	account, err := testQueries.CreateAccount(context.Background(), arg)
 	require.NoError(t, err)
 	require.NotEmpty(t, account)
-
 	require.Equal(t, arg.Owner, account.Owner)
 	require.Equal(t, arg.Balance, account.Balance)
 	require.Equal(t, arg.Currency, account.Currency)
-
 	require.NotZero(t, account.ID)
 	require.NotZero(t, account.CreatedAt)
 
